588d8a85adb80765a1f459688932559efb38fc4c
Fernando Serna <fernando@Fernandos-MBP.hsd1.wa.comcast.net>
2019-08-19 23:12:06 -0700

    updated weather app

---
+++ precache-manifest.db6d6aebdb6cecb2edb544355e8715a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b99be0f120652989de624bb0b227d742",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c05b542368392e3efaa",
+    "url": "/static/css/main.5d83feb8.chunk.css"
+  },
+  {
+    "revision": "a7180ea1d96efab26f11",
+    "url": "/static/js/2.d0fe70de.chunk.js"
+  },
+  {
+    "revision": "9c05b542368392e3efaa",
+    "url": "/static/js/main.c1b12ba5.chunk.js"
+  },
+  {
+    "revision": "ce69506db52c52de3b31",
+    "url": "/static/js/runtime~main.480bd28e.js"
+  }
+]);
---
+++ sw.js
@@ -0,0 +1,42 @@
+if ('function' === typeof importScripts) {
+  importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+}
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+
+  workbox.precaching.precacheAndRoute([
+  {
+    "url": "static/css/main.5d83feb8.chunk.css",
+    "revision": "6832c85d56f4824e54b679156a39d8a2"
+  },
+  {
+    "url": "index.html",
+    "revision": "b99be0f120652989de624bb0b227d742"
+  },
+  {
+    "url": "offline.html",
+    "revision": "09b9feaee1fbd9d3f27253d24b7911c9"
+  },
+  {
+    "url": "404.html",
+    "revision": "1a6cf0261a93d2c998c813d5588856bb"
+  }
+]);
+
+  workbox.routing.registerRoute(
+    new RegExp('https://cors-anywhere.herokuapp.com/'),
+    new workbox.strategies.NetworkFirst({
+      cacheName: 'weather-cache',
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 30,
+          maxAgeSeconds: 7 * 24 * 60 * 60
+        })
+      ]
+    })
+  )
+
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
